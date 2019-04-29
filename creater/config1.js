312a54ac5edd11fe4c5f6e292bb58215e190f12e
Steve Yu <steveyu@httpeace.com>
2019-04-29 18:55:49 +0800

    add example2

--- creater/config1.js
+++ creater/config1.js
@@ -1,15 +1,15 @@
 module.exports = {
   config: {
-    dir: "src"
+    dir: 'src/example1'
   },
   files: [
     {
-      path: "newFile.js",
-      content: "i am new file"
+      path: 'newFile.js',
+      content: 'i am new file'
     },
     {
-      path: "css/newFile.css",
-      content: "i am new css file in css folder"
+      path: 'css/newFile.css',
+      content: 'i am new css file in css folder'
     }
   ]
 };
---
+++ creater/config2.js
@@ -0,0 +1,15 @@
+module.exports = {
+  config: {
+    dir: 'src/example2'
+  },
+  files: [
+    {
+      path: 'actions.js',
+      format: []
+    },
+    {
+      path: 'types.js',
+      format: []
+    }
+  ]
+};
