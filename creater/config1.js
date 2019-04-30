49c4a184ddab776052e08da015dd85b734386b5d
Steve Yu <steveyu@httpeace.com>
2019-04-30 16:45:43 +0800

    update config1 example1

--- creater/config1.js
+++ creater/config1.js
@@ -1,3 +1,5 @@
+const {helper} = require('creater-cli');
+
 module.exports = {
   config: {
     dir: 'src/example1'
@@ -10,6 +12,24 @@ module.exports = {
     {
       path: 'css/newFile.css',
       content: 'i am new css file in css folder'
+    },
+    {
+      path: 'myCopyFile1.js',
+      content: '@path:../temp.js'
+    },
+    {
+      path: 'myCopyFile2.js',
+      content: helper.withPath('../temp.js') // Equate to '@path:temp.js'
+    },
+    {
+      path: 'myCopyFile3.js',
+      format: (prevContent, content) => content + '\ni am copy from temp.js',
+      content: helper.withPath('../temp.js')
+    },
+    {
+      path: 'myUpdateFile.js',
+      format: (prevContent, content) => prevContent + '\n' + content + '\n' + 'update some thing',
+      content: helper.withPath('../temp.js')
     }
   ]
 };
